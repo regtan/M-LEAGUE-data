98e1c9761f3927d6ba31e4cdf14722f4295dc958
regtan <regtan1210@gmail.com>
2024-01-28 01:24:37 +0900

    Add Team model, register in admin, add to installed apps

--- teams/models.py
+++ teams/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Team(models.Model):
+    name = models.CharField(max_length=255)
+    name_kana = models.CharField(max_length=255)
+    official_x_id = models.CharField(max_length=255)
+    join_date = models.DateField("date joined league")
