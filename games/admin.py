ef2b15fda0270d99afeb6c603f1c3eeb95d59242
regtan <regtan1210@gmail.com>
2024-02-07 00:07:51 +0900

    Add HaiPai model and register in admin panel

--- games/admin.py
+++ games/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Game, Round, GameBroadcaster, GameResult
+from .models import Game, Round, GameBroadcaster, GameResult, HaiPai
 
 admin.site.register(Game)
 admin.site.register(Round)
 admin.site.register(GameBroadcaster)
 admin.site.register(GameResult)
+admin.site.register(HaiPai)
