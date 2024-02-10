03b286edb0eab68eb667f3f6e0d7a6ceb6d201c2
regtan <regtan1210@gmail.com>
2024-02-10 23:16:53 +0900

    Refactor models and admin for better organization

--- rounds/admin.py
+++ rounds/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Round, HaiPai, WinningHand
 
-# Register your models here.
+admin.site.register(Round)
+admin.site.register(HaiPai)
+admin.site.register(WinningHand)
