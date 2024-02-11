93ec6e0cf15755de05e340e27280953bebbdb913
regtan <regtan1210@gmail.com>
2024-02-11 23:08:23 +0900

    Add DrawnRound model and register in admin panel

--- rounds/admin.py
+++ rounds/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Round, HaiPai, WinningHand
+from .models import DrawnRound, Round, HaiPai, WinningHand
 
 admin.site.register(Round)
 admin.site.register(HaiPai)
 admin.site.register(WinningHand)
+admin.site.register(DrawnRound)
